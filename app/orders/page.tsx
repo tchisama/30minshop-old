1e3bcadbf5c4e98b0c98ae398a670f0debfa5f6f
tchi-sama <tchisama41@gmail.com>
2023-06-25 02:18:38 +0100

    orders working prety good

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,6 +1,26 @@
+"use client"
 import { Menu } from "@/components/Menu";
-
+import { orderRef } from "@/firebase";
+import { useUser } from "@/store/useUser";
+import { onSnapshot, query, where } from "firebase/firestore";
+import {useState,useEffect} from "react"
 export default function Orders() {
+  const [orders,setOrders]=useState([])
+  const { store }= useUser()
+  useEffect(() => {
+    const q = query(orderRef,where("store","==",store))
+    const getData = onSnapshot(q,snapshot=>{
+      let ords = []
+      snapshot.docs.forEach(doc=>{
+        ords.push({...doc.data(),id:doc.id})
+      })
+      setOrders(ords)
+    })
+    return () => {
+      getData()
+    }
+  }, [])
+  
   return (
     <div className="min-h-screen w-full flex relative">
       <Menu />
@@ -11,17 +31,35 @@ export default function Orders() {
           {/* head */}
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>full name</th>
+              <th>phone number</th>
+              <th>address</th>
+              <th>products</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
+            {
+              orders.map((order:any)=>(
+                  <tr>
+                    <td>{order.fullName}</td>
+                    <td>{order.number}</td>
+                    <td>{order.address}</td>
+                    <td>
+                      <div className="avatar-group -space-x-6 py-1">
+                        {
+                          order.cart.map((o:any)=>(
+                        <div className="avatar border-[3px] shadow-md border-white">
+                          <div className="w-12">
+                                    <img className="object-contain bg-white" src={o.product.img} />
+                          </div>
+                        </div>
+                          ))
+                        }
+                      </div>
+                    </td>
+                  </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
